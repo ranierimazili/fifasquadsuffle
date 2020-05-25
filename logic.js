01bba42e9cfc59715bd80e2589fb79a7ec60cc8b
Ranieri Mazili <ranieri85@gmail.com>
2020-05-24 22:53:36 -0300

    first version

---
+++ logic.js
@@ -0,0 +1,375 @@
+var estrutura = {
+	"participantes" : [
+		"Jian"
+		,"Rafael"
+		,"Magnun"
+		,"Daniel"
+		,"Léo Batista"
+		,"Dudu"
+		,"Jesse"
+		,"Leo"
+		,"Ariam"
+		,"Jammes"
+		,"Hélio"
+		,"Cristiano Lopes"
+		,"Ranieri"
+		,"Willian Martins"
+	],
+	"times" : [
+	  "FC Barcelona",
+	  "Real Madrid",
+	  "Juventus",
+	  "Paris Saint-Germain",
+	  "Manchester City",
+	  "Liverpool",
+	  "FC Bayern München",
+	  "Atlético Madrid",
+	  "Tottenham Hotspur",
+	  "Napoli",
+	  "Borussia Dortmund",
+	  "Inter",
+	  "Manchester United",
+	  "Lazio",
+	  "Valencia CF",
+	  "Chelsea",
+	  "Arsenal",
+	  "RB Leipzig",
+	  "Real Betis",
+	  "Bayer 04 Leverkusen"
+	],
+	"posicoes" : [
+		"Goleiro"
+		,"Lateral Direito"
+		,"Zagueiro 1"
+		,"Zagueiro 2"
+		,"Lateral Esquerdo"
+		,"Volante 1"
+		,"Volante 2"
+		,"Meio Campo"
+		,"Atacante 1"
+		,"Atacante 2"
+		,"Atacante 3"
+	],
+	"jogadores" : {
+		"Goleiro" : [
+		  "Jan Oblak - (Atlético Madrid)",
+		  "Alisson - (Liverpool)",
+		  "Marc-André ter Stegen - (FC Barcelona)",
+		  "Manuel Neuer - (FC Bayern München)",
+		  "Ederson - (Manchester City)",
+		  "De Gea - (Manchester United)",
+		  "Thibaut Courtois - (Real Madrid)",
+		  "Samir Handanovič - (Inter)",
+		  "Keylor Navas - (Paris Saint-Germain)",
+		  "Wojciech Szczęsny - (Juventus)",
+		  "Hugo Lloris - (Tottenham Hotspur)",
+		  "Yann Sommer - (Borussia Mönchengladbach)",
+		  "Gianluigi Donnarumma - (Milan)",
+		  "André Onana - (Ajax)",
+		  "Bernd Leno - (Arsenal)",
+		  "Roman Bürki - (Borussia Dortmund)",
+		  "Péter Gulácsi - (RB Leipzig)",
+		  "Anthony Lopes - (Olympique Lyonnais)",
+		  "Neto - (FC Barcelona)",
+		  "Salvatore Sirigu - (Torino)"
+		],
+		"Lateral Direito" : [
+		  "Joshua Kimmich - (FC Bayern München)",
+		  "Trent Alexander-Arnold - (Liverpool)",
+		  "Carvajal - (Real Madrid)",
+		  "Ricardo Pereira - (Leicester City)",
+		  "Azpilicueta - (Chelsea)",
+		  "Juan Cuadrado - (Juventus)",
+		  "Kyle Walker - (Manchester City)",
+		  "Achraf HakimiL - (Borussia Dortmund)",
+		  "Aaron Wan-Bissaka - (Manchester United)",
+		  "Nélson Semedo - (FC Barcelona)",
+		  "Joe Gomez - (Liverpool)",
+		  "João Cancelo - (Manchester City)",
+		  "Nacho Fernández - (Real Madrid)",
+		  "Sergi Roberto - (FC Barcelona)",
+		  "Jesús Corona - (FC Porto)",
+		  "Jesús Navas - (Sevilla FC)",
+		  "Éder Militão - (Real Madrid)",
+		  "Lukas Klostermann - (RB Leipzig)",
+		  "Alessandro FlorenziL - (Valencia CF)",
+		  "Thomas Meunier - (Paris Saint-Germain)"
+		],
+		"Lateral Esquerdo" : [
+		  "Andrew Robertson - (Liverpool)",
+		  "Jordi Alba - (FC Barcelona)",
+		  "Saúl - (Atlético Madrid)",
+		  "Jan Vertonghen - (Tottenham Hotspur)",
+		  "Lucas Hernández - (FC Bayern München)",
+		  "Alex Telles - (FC Porto)",
+		  "Alex Sandro - (Juventus)",
+		  "Azpilicueta - (Chelsea)",
+		  "Marcelo - (Real Madrid)",
+		  "Nicolás Tagliafico - (Ajax)",
+		  "Grimaldo - (SL Benfica)",
+		  "Raphaël Guerreiro - (Borussia Dortmund)",
+		  "Lucas Digne - (Everton)",
+		  "David Alaba - (FC Bayern München)",
+		  "Daley Blind - (Ajax)",
+		  "Achraf HakimiL - (Borussia Dortmund)",
+		  "Marcos Acuña - (Sporting CP)",
+		  "Nacho Fernández - (Real Madrid)",
+		  "Aleksandar Kolarov - (Roma)",
+		  "Ben Chilwell - (Leicester City)"
+		],
+		"Zagueiro 1" : [
+		  "Virgil van Dijk - (Liverpool)",
+		  "Sergio Ramos - (Real Madrid)",
+		  "Kalidou Koulibaly - (Napoli)",
+		  "Piqué - (FC Barcelona)",
+		  "Giorgio Chiellini - (Juventus)",
+		  "Aymeric Laporte - (Manchester City)",
+		  "Diego Godín - (Inter)",
+		  "Mats Hummels - (Borussia Dortmund)",
+		  "Thiago Silva - (Paris Saint-Germain)",
+		  "Milan Škriniar - (Inter)",
+		  "Clément Lenglet - (FC Barcelona)",
+		  "Marquinhos - (Paris Saint-Germain)",
+		  "Raphaël Varane - (Real Madrid)",
+		  "Leonardo Bonucci - (Juventus)",
+		  "Toby Alderweireld - (Tottenham Hotspur)",
+		  "Matthijs de Ligt - (Juventus)",
+		  "José María Giménez - (Atlético Madrid)",
+		  "Niklas Süle - (FC Bayern München)",
+		  "Samuel Umtiti - (FC Barcelona)",
+		  "Stefan de Vrij - (Inter)"
+		],
+		"Zagueiro 2" : [
+		  "Jan Vertonghen - (Tottenham Hotspur)",
+		  "Fernandinho - (Manchester City)",
+		  "Lucas Hernández - (FC Bayern München)",
+		  "Felipe - (Atlético Madrid)",
+		  "Francesco Acerbi - (Lazio)",
+		  "Kostas Manolas - (Napoli)",
+		  "Azpilicueta - (Chelsea)",
+		  "Josué Chiamulera - (Grêmio)",
+		  "Dakonam Djené - (Getafe CF)",
+		  "Alessio Romagnoli - (Milan)",
+		  "Stefan Savić - (Atlético Madrid)",
+		  "Harry Maguire - (Manchester United)",
+		  "Gabriel Paulista - (Valencia CF)",
+		  "Bartra - (Real Betis)",
+		  "David Alaba - (FC Bayern München)",
+		  "Joel Matip - (Liverpool)",
+		  "Daley Blind - (Ajax)",
+		  "Ezequiel Garay - (Valencia CF)",
+		  "Raúl Albiol - (Villarreal CF)",
+		  "Rúben Dias - (SL Benfica)"
+		],
+		"Volante 1" : [
+		  "N'Golo Kanté - (Chelsea)",
+		  "Casemiro - (Real Madrid)",
+		  "Sergio Busquets - (FC Barcelona)",
+		  "Pierre-Emerick Aubameyang - (Arsenal)",
+		  "Joshua Kimmich - (FC Bayern München)",
+		  "Heung Min Son - (Tottenham Hotspur)",
+		  "Marco Verratti - (Paris Saint-Germain)",
+		  "Paul Pogba - (Manchester United)",
+		  "Christian Eriksen - (Inter)",
+		  "Jadon Sancho - (Borussia Dortmund)",
+		  "Fabinho - (Liverpool)",
+		  "Marquinhos - (Paris Saint-Germain)",
+		  "Riyad Mahrez - (Manchester City)",
+		  "Lorenzo Insigne - (Napoli)",
+		  "Koke - (Atlético Madrid)",
+		  "Thiago - (FC Bayern München)",
+		  "Miralem Pjanić - (Juventus)",
+		  "Rodri - (Manchester City)",
+		  "Sergej Milinković-Savić - (Lazio)",
+		  "Nabil Fekir - (Real Betis)"
+		],
+		"Volante 2" : [
+		  "Saúl - (Atlético Madrid)",
+		  "Serge Gnabry - (FC Bayern München)",
+		  "Pizzi - (SL Benfica)",
+		  "Thomas Müller - (FC Bayern München)",
+		  "Paulinho - (Guangzhou Evergrande Taobao FC)",
+		  "Jordan Henderson - (Liverpool)",
+		  "Georginio Wijnaldum - (Liverpool)",
+		  "Axel Witsel - (Borussia Dortmund)",
+		  "Fernandinho - (Manchester City)",
+		  "Kai Havertz - (Bayer 04 Leverkusen)",
+		  "Marcus Rashford - (Manchester United)",
+		  "Wilfred Ndidi - (Leicester City)",
+		  "Kingsley Coman - (FC Bayern München)",
+		  "Julian Brandt - (Borussia Dortmund)",
+		  "Thomas Partey - (Atlético Madrid)",
+		  "Leon Goretzka - (FC Bayern München)",
+		  "Jorginho - (Chelsea)",
+		  "James Rodríguez - (Real Madrid)",
+		  "Idrissa Gueye - (Paris Saint-Germain)",
+		  "Alex Sandro - (Juventus)"
+		],
+		"Meio Campo" : [
+		  "Kevin De Bruyne - (Manchester City)",
+		  "N'Golo Kanté - (Chelsea)",
+		  "Toni Kroos - (Real Madrid)",
+		  "Luka Modrić - (Real Madrid)",
+		  "Bernardo Silva - (Manchester City)",
+		  "Joshua Kimmich - (FC Bayern München)",
+		  "Paulo Dybala - (Juventus)",
+		  "Marco Verratti - (Paris Saint-Germain)",
+		  "Paul Pogba - (Manchester United)",
+		  "Christian Eriksen - (Inter)",
+		  "Parejo - (Valencia CF)",
+		  "Marco Reus - (Borussia Dortmund)",
+		  "David Silva - (Manchester City)",
+		  "Jadon Sancho - (Borussia Dortmund)",
+		  "Frenkie de Jong - (FC Barcelona)",
+		  "Bruno Fernandes - (Manchester United)",
+		  "Hakim Ziyech - (Ajax)",
+		  "Koke - (Atlético Madrid)",
+		  "Thiago - (FC Bayern München)",
+		  "Miralem Pjanić - (Juventus)"
+		],
+		"Atacante 1" : [
+		  "Lionel Messi - (FC Barcelona)",
+		  "Cristiano Ronaldo - (Juventus)",
+		  "Neymar Jr - (Paris Saint-Germain)",
+		  "Robert Lewandowski - (FC Bayern München)",
+		  "Eden Hazard - (Real Madrid)",
+		  "Mohamed Salah - (Liverpool)",
+		  "Sadio Mané - (Liverpool)",
+		  "Sergio Agüero - (Manchester City)",
+		  "Kylian Mbappé - (Paris Saint-Germain)",
+		  "Harry Kane - (Tottenham Hotspur)",
+		  "Antoine Griezmann - (FC Barcelona)",
+		  "Luis Suárez - (FC Barcelona)",
+		  "Raheem Sterling - (Manchester City)",
+		  "Pierre-Emerick Aubameyang - (Arsenal)",
+		  "Karim Benzema - (Real Madrid)",
+		  "Bernardo Silva - (Manchester City)",
+		  "Ciro Immobile - (Lazio)",
+		  "Ángel Di María - (Paris Saint-Germain)",
+		  "Dries Mertens - (Napoli)",
+		  "Leroy Sané - (Manchester City)"
+		],
+		"Atacante 2" : [
+		  "Timo Werner - (RB Leipzig)",
+		  "Jamie Vardy - (Leicester City)",
+		  "Hakim Ziyech - (Ajax)",
+		  "Riyad Mahrez - (Manchester City)",
+		  "Mauro IcardiL - (Paris Saint-Germain)",
+		  "Lorenzo Insigne - (Napoli)",
+		  "Romelu Lukaku - (Inter)",
+		  "Edinson Cavani - (Paris Saint-Germain)",
+		  "Alejandro Gómez - (Atalanta)",
+		  "Nabil Fekir - (Real Betis)",
+		  "Serge Gnabry - (FC Bayern München)",
+		  "Isco - (Real Madrid)",
+		  "Iago Aspas - (RC Celta)",
+		  "Thomas Müller - (FC Bayern München)",
+		  "CoutinhoL - (FC Bayern München)",
+		  "Gareth Bale - (Real Madrid)",
+		  "Gonzalo Higuaín - (Juventus)",
+		  "Zlatan Ibrahimović - (Milan)",
+		  "Marcus Rashford - (Manchester United)",
+		  "Lautaro Martínez - (Inter)"
+		],
+		"Atacante 3" : [
+   		  "Kingsley Coman - (FC Bayern München)",
+		  "Memphis Depay - (Olympique Lyonnais)",
+		  "Josip Iličić - (Atalanta)",
+		  "Wissam Ben Yedder - (AS Monaco Football Club SA)",
+		  "Alexandre Lacazette - (Arsenal)",
+		  "Douglas Costa - (Juventus)",
+		  "Oscar - (Shanghai SIPG FC)",
+		  "José Callejón - (Napoli)",
+		  "Edin Džeko - (Roma)",
+		  "Ousmane Dembélé - (FC Barcelona)",
+		  "Ronaldo Cabrais - (Grêmio)",
+		  "Louri Beretta - (Atlético Mineiro)",
+		  "Oyarzabal - (Real Sociedad)",
+		  "Andrej Kramarić - (TSG 1899 Hoffenheim)",
+		  "Duván Zapata - (Atalanta)",
+		  "Anderson Talisca - (Guangzhou Evergrande Taobao FC)",
+		  "Anthony Martial - (Manchester United)",
+		  "Quincy Promes - (Ajax)",
+		  "Arkadiusz Milik - (Napoli)",
+		  "Portu - (Real Sociedad)"
+		]
+	}
+}
+
+var results = [];
+
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+function escolherTimes() {
+
+	//Escolha dos times
+	console.log("=================================")
+	console.log("=== Escolha dos times ===========")
+	console.log("=================================")
+	var competidores = shuffle(estrutura.participantes);
+	for (var i=0;i<competidores.length;i++) {
+		console.log("O competidor " + competidores[i] + " jogara com time " + estrutura.times[i]);
+		results[competidores[i]] = {};
+		results[competidores[i]].nome = competidores[i];
+		results[competidores[i]].time = estrutura.times[i];
+		results[competidores[i]].jogadores = [];
+	}
+
+}
+
+function escolherJogadores() {
+	//Escolha dos jogadores
+	for (var i=0;i<estrutura.posicoes.length;i++) {
+		console.log("=====================================")
+		console.log("= Posicao a ser escolhida: " + estrutura.posicoes[i]);
+		console.log("=====================================")
+		
+		var competidores = shuffle(estrutura.participantes);
+		for (var j=0;j<competidores.length;j++) {
+			console.log("O competidor " + competidores[j] + " jogara com o " + estrutura.posicoes[i] + " - " + estrutura.jogadores[estrutura.posicoes[i]][j]);
+			results[competidores[j]].jogadores.push(estrutura.posicoes[i] + " - " + estrutura.jogadores[estrutura.posicoes[i]][j]);
+		}
+	}
+
+}
+
+function gerarTimes() {
+	document.getElementById('resultados').innerHTML = "";
+	results = [];
+	escolherTimes();
+	escolherJogadores();
+	imprimirResultados();
+}
+
+
+//Imprimir resultados
+function imprimirResultados() {
+	//console.log("chegou");
+	//results.forEach(imprimirJogador);
+	for (var key in results) {
+		document.getElementById('resultados').innerHTML += "<b>" + results[key].nome + " - " + results[key].time + "<br></b>";
+		for (var i=0;i<results[key].jogadores.length;i++) {
+			document.getElementById('resultados').innerHTML += results[key].jogadores[i] + "<br>";
+		}
+		document.getElementById('resultados').innerHTML += "<br><br>"
+	}
+}
+
+
